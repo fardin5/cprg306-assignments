d2be14aa31f08abbc31b54673f3ef6f35914774d
fardin5 <119972067+fardin5@users.noreply.github.com>
2023-10-22 20:57:36 -0600

    first commit

---
+++ app/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Page() {
+    return (
+        <div>
+            <p>Fardin</p>
+            <p>Your course section: CPRG 306 D</p>
+            <Link href="https://github.com/fardin5">My GitHub Repository</Link>
+        </div>
+    )
+}
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from 'app/StudentInfo';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>My Shopping List</h1>
+      <div><StudentInfo /></div>
+    </main>
+  )
+}
